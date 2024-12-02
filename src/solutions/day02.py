6864e0a491577b750e82466df2291632aa3274f2
carokaffee <carolin.kaffine@gmail.com>
2024-12-02 06:47:11 +0100

    day 2

--- src/solutions/day02.py
+++ src/solutions/day02.py
@@ -1,7 +1,58 @@
 from src.tools.loader import load_data
 
-TESTING = True
+TESTING = False
 
 
+#if True:
 if __name__ == "__main__":
+    print("hello")
     data = load_data(TESTING, "\n")
+
+    reports = []
+    for line in data:
+        #print(line)
+        report = list(map(int,line.split()))
+        reports.append(report)
+
+    new_reports = []
+    for line in data:
+        report_cluster = []
+        report = list(map(int, line.split()))
+        for i in range(len(report)):
+            mod_report = report.copy()
+            del mod_report[i]
+            report_cluster.append(mod_report)
+        report_cluster.append(report)
+        new_reports.append(report_cluster)
+
+    #print(reports)
+    print(new_reports)
+
+    count = 0
+
+    for reports2 in new_reports:
+        safe_in_general = False
+        for report in reports2:
+            safe = True
+            increase = True
+            if report[0] == report[1]:
+                safe = False
+            elif report[0] > report[1]:
+                increase = False
+            for i in range(len(report)-1):
+                if report[i] == report[i+1]:
+                    safe = False
+                elif abs(report[i] - report[i+1]) not in [1,2,3]:
+                    safe = False
+                elif report[i] > report[i+1] and increase == True:
+                    safe = False
+                elif report[i] < report[i+1] and increase == False:
+                    safe = False
+            #print('###', report, safe)
+            if safe:
+                safe_in_general = True
+            #print(safe_in_general)
+        if safe_in_general:
+            count += 1
+
+    print(count)
