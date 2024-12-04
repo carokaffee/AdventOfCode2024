550bab8434e61e137906800714b06ef78f2f90a1
carokaffee <carolin.kaffine@gmail.com>
2024-12-04 17:42:34 +0100

    day 3 + 4

--- src/solutions/day02.py
+++ src/solutions/day02.py
@@ -3,18 +3,35 @@ from src.tools.loader import load_data
 TESTING = False
 
 
-#if True:
-if __name__ == "__main__":
-    print("hello")
-    data = load_data(TESTING, "\n")
-
+def parse_input(data):
     reports = []
     for line in data:
-        #print(line)
         report = list(map(int,line.split()))
         reports.append(report)
+    return reports
 
-    new_reports = []
+
+def check_safety(report):
+    safe = True
+    increase = True
+    if report[0] == report[1]:
+        safe = False
+    elif report[0] > report[1]:
+        increase = False
+    for i in range(len(report)-1):
+        if report[i] == report[i+1]:
+            safe = False
+        elif abs(report[i] - report[i+1]) not in [1,2,3]:
+            safe = False
+        elif report[i] > report[i+1] and increase == True:
+            safe = False
+        elif report[i] < report[i+1] and increase == False:
+            safe = False
+    return safe
+
+
+def reports_with_error_tolerance(reports):
+    tolerance_reports = []
     for line in data:
         report_cluster = []
         report = list(map(int, line.split()))
@@ -22,36 +39,22 @@ if __name__ == "__main__":
             mod_report = report.copy()
             del mod_report[i]
             report_cluster.append(mod_report)
-        report_cluster.append(report)
-        new_reports.append(report_cluster)
+        tolerance_reports.append(report_cluster)
+    return tolerance_reports
 
-    #print(reports)
-    print(new_reports)
+
+if __name__ == "__main__":
+    data = load_data(TESTING, "\n")
+    reports = parse_input(data)
+    tolerance_reports = reports_with_error_tolerance(reports)
 
     count = 0
 
-    for reports2 in new_reports:
+    for reports2 in tolerance_reports:
         safe_in_general = False
         for report in reports2:
-            safe = True
-            increase = True
-            if report[0] == report[1]:
-                safe = False
-            elif report[0] > report[1]:
-                increase = False
-            for i in range(len(report)-1):
-                if report[i] == report[i+1]:
-                    safe = False
-                elif abs(report[i] - report[i+1]) not in [1,2,3]:
-                    safe = False
-                elif report[i] > report[i+1] and increase == True:
-                    safe = False
-                elif report[i] < report[i+1] and increase == False:
-                    safe = False
-            #print('###', report, safe)
-            if safe:
+            if check_safety(report):
                 safe_in_general = True
-            #print(safe_in_general)
         if safe_in_general:
             count += 1
 
--- src/solutions/day04.py
+++ src/solutions/day04.py
@@ -1,7 +1,77 @@
 from src.tools.loader import load_data
 
-TESTING = True
+TESTING = False
+
+
+#def parse_input(data):
+#    for line in data:
+
 
 
 if __name__ == "__main__":
-    data = load_data(TESTING, "\n")
+    puzzle = load_data(TESTING, "\n")
+
+    big_puzzle = ['' for _ in range(len(puzzle)+8)]
+    for i in range(4):
+        big_puzzle[i] = '.' * (len(puzzle[0])+8)
+        big_puzzle[-i-1] = '.' * (len(puzzle[0])+8)
+
+    for i,line in enumerate(puzzle):
+        big_puzzle[i+4] += '....' + line + '....'
+
+    xmas_count = 0
+
+    for i in range(4, len(big_puzzle)-3):
+        for j in range(4, len(big_puzzle[0])-3):
+            rightdown = ''
+            rightup = ''
+            leftdown = ''
+            leftup = ''
+            left = ''
+            right = ''
+            up = ''
+            down = ''
+            for x in range(4):
+                rightdown += big_puzzle[i+x][j+x]
+                leftup += big_puzzle[i-x][j-x]
+                rightup += big_puzzle[i-x][j+x]
+                leftdown += big_puzzle[i+x][j-x]
+                left += big_puzzle[i][j-x]
+                right += big_puzzle[i][j+x]
+                up += big_puzzle[i-x][j]
+                down += big_puzzle[i+x][j]
+            xmas_count += (rightdown == 'XMAS')
+            xmas_count += (rightup == 'XMAS')
+            xmas_count += (leftdown == 'XMAS')
+            xmas_count += (leftup == 'XMAS')
+            xmas_count += (left == 'XMAS')
+            xmas_count += (right == 'XMAS')
+            xmas_count += (up == 'XMAS')
+            xmas_count += (down == 'XMAS')
+
+    print(xmas_count)
+
+    rightdown_coords = set()
+    rightup_coords = set()
+
+    for i in range(4, len(big_puzzle)-3):
+        for j in range(4, len(big_puzzle[0])-3):
+            rightdown = ''
+            rightup = ''
+            leftdown = ''
+            leftup = ''
+            for x in range(3):
+                rightdown += big_puzzle[i+x][j+x]
+                leftup += big_puzzle[i-x][j-x]
+                rightup += big_puzzle[i-x][j+x]
+                leftdown += big_puzzle[i+x][j-x]
+            if rightdown == 'MAS':
+                rightdown_coords.add((i+1,j+1))
+            if leftup == 'MAS':
+                rightdown_coords.add((i-1,j-1))
+            if rightup == 'MAS':
+                rightup_coords.add((i-1,j+1))
+            if leftdown == 'MAS':
+                rightup_coords.add((i+1,j-1))
+
+    print(len(rightdown_coords.intersection(rightup_coords)))
