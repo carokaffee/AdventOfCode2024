98bed330db5bbccc4c44001aef3e9256e7869fe0
carokaffee <carolin.kaffine@gmail.com>
2024-12-07 17:00:15 +0100

    day 2 clean

--- src/solutions/day02.py
+++ src/solutions/day02.py
@@ -6,56 +6,36 @@ TESTING = False
 def parse_input(data):
     reports = []
     for line in data:
-        report = list(map(int,line.split()))
-        reports.append(report)
+        reports.append(list(map(int,line.split())))
     return reports
 
 
 def check_safety(report):
-    safe = True
-    increase = True
-    if report[0] == report[1]:
-        safe = False
-    elif report[0] > report[1]:
-        increase = False
-    for i in range(len(report)-1):
-        if report[i] == report[i+1]:
-            safe = False
-        elif abs(report[i] - report[i+1]) not in [1,2,3]:
-            safe = False
-        elif report[i] > report[i+1] and increase == True:
-            safe = False
-        elif report[i] < report[i+1] and increase == False:
-            safe = False
-    return safe
+    differences = [report[i+1] - report[i] for i in range(len(report)-1)]
+    return set(differences).issubset([1,2,3]) or set(differences).issubset([-1,-2,-3])
 
 
-def reports_with_error_tolerance(reports):
-    tolerance_reports = []
-    for line in data:
-        report_cluster = []
-        report = list(map(int, line.split()))
-        for i in range(len(report)):
-            mod_report = report.copy()
-            del mod_report[i]
-            report_cluster.append(mod_report)
-        tolerance_reports.append(report_cluster)
-    return tolerance_reports
+def count_safe_reports(reports):
+    safe_reports = sum([check_safety(report) for report in reports])
+    return safe_reports
 
+
+def count_safe_reports_with_tolerance(reports):
+    tolerance_reports = [[report[:i] + report[i+1:] for i in range(len(report))] for report in reports]
+    safe_reports = sum([any([check_safety(report) for report in reports]) for reports in tolerance_reports])
+    return safe_reports
+    
 
 if __name__ == "__main__":
     data = load_data(TESTING, "\n")
     reports = parse_input(data)
-    tolerance_reports = reports_with_error_tolerance(reports)
 
-    count = 0
+    # PART 1
+    # test:     2
+    # answer: 321
+    print(count_safe_reports(reports))
 
-    for reports2 in tolerance_reports:
-        safe_in_general = False
-        for report in reports2:
-            if check_safety(report):
-                safe_in_general = True
-        if safe_in_general:
-            count += 1
-
-    print(count)
+    # PART 1
+    # test:     4
+    # answer: 386
+    print(count_safe_reports_with_tolerance(reports))
