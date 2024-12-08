9ec470eec57e661ca3a266e187cff0012a9862d4
carokaffee <carolin.kaffine@gmail.com>
2024-12-08 15:48:57 +0100

    day 4 clean

--- src/solutions/day04.py
+++ src/solutions/day04.py
@@ -3,75 +3,59 @@ from src.tools.loader import load_data
 TESTING = False
 
 
-#def parse_input(data):
-#    for line in data:
+def padded_puzzle(puzzle):
+    width = len(puzzle[0]) + 8
+    padding_line = '.' * width
+    padded_puzzle = [padding_line] * 4
 
+    for line in puzzle:
+        padded_puzzle.append('....' + line + '....')
+    padded_puzzle += [padding_line] * 4
+
+    return padded_puzzle
+
+
+def count_XMAS(puzzle):
+    xmas_count = 0
+    directions = [(0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1)]
+    for x in range(4, len(puzzle)-3):
+        for y in range(4, len(puzzle[0])-3):
+            for dx, dy in directions:
+                xmas = ''.join([puzzle[x + dx * n][y + dy * n] for n in range(4)])
+                if xmas == 'XMAS':
+                    xmas_count += 1
+    return xmas_count
+
+
+def count_X_MAS(puzzle):
+    down_coords = set()
+    up_coords = set()
+    
+
+    for x in range(4, len(puzzle)-3):
+        for y in range(4, len(puzzle[0])-3):
+            for dx, dy in [(1,1), (-1,-1)]:
+                mas = ''.join([puzzle[x + dx * n][y + dy * n] for n in range(3)])
+                if mas == 'MAS':
+                    down_coords.add((x + dx,y + dy))
+            for dx, dy in [(1,-1), (-1,1)]:
+                mas = ''.join([puzzle[x + dx * n][y + dy * n] for n in range(3)])
+                if mas == 'MAS':
+                    up_coords.add((x + dx,y + dy))
+
+    return len(down_coords.intersection(up_coords))
 
 
 if __name__ == "__main__":
-    puzzle = load_data(TESTING, "\n")
+    data = load_data(TESTING, "\n")
+    puzzle = padded_puzzle(data)
+    
+    # PART 1
+    # test:     18
+    # answer: 2644
+    print(count_XMAS(puzzle))
 
-    big_puzzle = ['' for _ in range(len(puzzle)+8)]
-    for i in range(4):
-        big_puzzle[i] = '.' * (len(puzzle[0])+8)
-        big_puzzle[-i-1] = '.' * (len(puzzle[0])+8)
-
-    for i,line in enumerate(puzzle):
-        big_puzzle[i+4] += '....' + line + '....'
-
-    xmas_count = 0
-
-    for i in range(4, len(big_puzzle)-3):
-        for j in range(4, len(big_puzzle[0])-3):
-            rightdown = ''
-            rightup = ''
-            leftdown = ''
-            leftup = ''
-            left = ''
-            right = ''
-            up = ''
-            down = ''
-            for x in range(4):
-                rightdown += big_puzzle[i+x][j+x]
-                leftup += big_puzzle[i-x][j-x]
-                rightup += big_puzzle[i-x][j+x]
-                leftdown += big_puzzle[i+x][j-x]
-                left += big_puzzle[i][j-x]
-                right += big_puzzle[i][j+x]
-                up += big_puzzle[i-x][j]
-                down += big_puzzle[i+x][j]
-            xmas_count += (rightdown == 'XMAS')
-            xmas_count += (rightup == 'XMAS')
-            xmas_count += (leftdown == 'XMAS')
-            xmas_count += (leftup == 'XMAS')
-            xmas_count += (left == 'XMAS')
-            xmas_count += (right == 'XMAS')
-            xmas_count += (up == 'XMAS')
-            xmas_count += (down == 'XMAS')
-
-    print(xmas_count)
-
-    rightdown_coords = set()
-    rightup_coords = set()
-
-    for i in range(4, len(big_puzzle)-3):
-        for j in range(4, len(big_puzzle[0])-3):
-            rightdown = ''
-            rightup = ''
-            leftdown = ''
-            leftup = ''
-            for x in range(3):
-                rightdown += big_puzzle[i+x][j+x]
-                leftup += big_puzzle[i-x][j-x]
-                rightup += big_puzzle[i-x][j+x]
-                leftdown += big_puzzle[i+x][j-x]
-            if rightdown == 'MAS':
-                rightdown_coords.add((i+1,j+1))
-            if leftup == 'MAS':
-                rightdown_coords.add((i-1,j-1))
-            if rightup == 'MAS':
-                rightup_coords.add((i-1,j+1))
-            if leftdown == 'MAS':
-                rightup_coords.add((i+1,j-1))
-
-    print(len(rightdown_coords.intersection(rightup_coords)))
+    # PART 2
+    # test:      9
+    # answer: 1952
+    print(count_X_MAS(puzzle))
