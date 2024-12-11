4acdfaab1e44ba1bb34a4d1ebdae5176423b307b
carokaffee <carolin.kaffine@gmail.com>
2024-12-11 10:01:48 +0100

    day 6

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ My solutions for AoC 2024 written in Python.
 | 03 | [Mull It Over](https://adventofcode.com/2024/day/3) | [day03.py](src/solutions/day03.py) || 16 | [](https://adventofcode.com/2024/day/16) | [](src/solutions/day16.py) |
 | 04 | [Ceres Search](https://adventofcode.com/2024/day/4) | [day04.py](src/solutions/day04.py) || 17 | [](https://adventofcode.com/2024/day/17) | [](src/solutions/day17.py) |
 | 05 | [Print Queue](https://adventofcode.com/2024/day/5) | [day05.py](src/solutions/day05.py) || 18 | [](https://adventofcode.com/2024/day/18) | [](src/solutions/day18.py) |
-| 06 | [Guard Gallivant](https://adventofcode.com/2024/day/6) | [](src/solutions/day06.py) || 19 | [](https://adventofcode.com/2024/day/19) | [](src/solutions/day19.py) |
+| 06 | [Guard Gallivant](https://adventofcode.com/2024/day/6) | [day06.py](src/solutions/day06.py) || 19 | [](https://adventofcode.com/2024/day/19) | [](src/solutions/day19.py) |
 | 07 | [Bridge Repair](https://adventofcode.com/2024/day/7) | [day07.py](src/solutions/day07.py) || 20 | [](https://adventofcode.com/2024/day/20) | [](src/solutions/day20.py) |
 | 08 | [Resonant Collinearity](https://adventofcode.com/2024/day/8) | [day08.py](src/solutions/day08.py) || 21 | [](https://adventofcode.com/2024/day/21) | [](src/solutions/day21.py) |
 | 09 | [Disk Fragmenter](https://adventofcode.com/2024/day/9) | [](src/solutions/day09.py) || 22 | [](https://adventofcode.com/2024/day/22) | [](src/solutions/day22.py) |
--- src/solutions/day06.py
+++ src/solutions/day06.py
@@ -1,75 +1,78 @@
 from src.tools.loader import load_data
 
-TESTING = True
+TESTING = False
+
+
+def parse_input(data):
+    grid = [line for line in data]
+    start = "".join(data).find("^") // len(data) + ("".join(data).find("^") % len(data)) * 1j
+    return grid, start
+
+
+def simulate_guard(rows, start):
+    length = len(data)
+    width = len(data[0])
+
+    current_dir = -1 + 0j
+    current_pos = start
+    visited = set([current_pos])
+    guard_left = False
+
+    while not guard_left:
+        new_pos = current_pos + current_dir
+        new_x = int(new_pos.real)
+        new_y = int(new_pos.imag)
+        if new_x < 0 or new_x >= length or new_y < 0 or new_y >= width:
+            guard_left = True
+        elif rows[new_x][new_y] == "#":
+            current_dir *= -1j
+        else:
+            current_pos = new_pos
+            visited.add(new_pos)
+
+    return visited
+
+
+def count_loops(rows, start, guard_path):
+    length = len(data)
+    width = len(data[0])
+    loops_found = 0
+
+    for coord in guard_path:
+        current_dir = -1 + 0j
+        current_pos = start
+        visited = set([current_pos, current_dir])
+        guard_left = False
+
+        while not guard_left:
+            new_pos = current_pos + current_dir
+            new_x = int(new_pos.real)
+            new_y = int(new_pos.imag)
+            if (new_pos, current_dir) in visited:
+                loops_found += 1
+                guard_left = True
+            elif new_x < 0 or new_x >= length or new_y < 0 or new_y >= width:
+                guard_left = True
+            elif rows[new_x][new_y] in "#" or new_x + new_y * 1j == coord:
+                current_dir *= -1j
+            else:
+                current_pos = new_pos
+                visited.add((new_pos, current_dir))
+
+    return loops_found
 
 
 if __name__ == "__main__":
     data = load_data(TESTING, "\n")
+    grid, start = parse_input(data)
 
-    gone = False
+    # PART 1
+    # test:     41
+    # answer: 5305
+    guard_path = simulate_guard(grid, start)
+    print(len(guard_path))
 
-    directions = {"v": (1, 0), "^": (-1, 0), "<": (0, -1), ">": (0, 1)}
-
-    for i in range(len(data)):
-        for j in range(len(data[0])):
-            if data[i][j] in directions.keys():
-                position = (i, j)
-                fixed_position = (i, j)
-
-    visited = [position]
-
-    direction = directions[data[position[0]][position[1]]]
-    fixed_direction = direction
-
-    while not gone:
-        new_position = (position[0] + direction[0], position[1] + direction[1])
-        if new_position[0] not in range(len(data)) or new_position[1] not in range(len(data[0])):
-            gone = True
-        else:
-            if data[new_position[0]][new_position[1]] in [".", "<", ">", "^", "v"]:
-                position = new_position
-                visited.append(new_position)
-            else:
-                if direction == (1, 0):
-                    direction = (0, -1)
-                elif direction == (-1, 0):
-                    direction = (0, 1)
-                elif direction == (0, -1):
-                    direction = (-1, 0)
-                elif direction == (0, 1):
-                    direction = (1, 0)
-
-    print(len(set(visited)))
-
-    loop_positions = []
-
-    for i, visit in enumerate(visited):
-        print(i)
-        if visit != fixed_position:
-            position = fixed_position
-            direction = fixed_direction
-            new_visits = [(position[0], position[1], direction)]
-            done = False
-            while not done:
-                new_position = (position[0] + direction[0], position[1] + direction[1])
-                if new_position[0] not in range(len(data)) or new_position[1] not in range(len(data[0])):
-                    done = True
-                else:
-                    if data[new_position[0]][new_position[1]] in [".", "<", ">", "^", "v"] and new_position != visit:
-                        position = new_position
-                    else:
-                        if direction == (1, 0):
-                            direction = (0, -1)
-                        elif direction == (-1, 0):
-                            direction = (0, 1)
-                        elif direction == (0, -1):
-                            direction = (-1, 0)
-                        elif direction == (0, 1):
-                            direction = (1, 0)
-
-                if (new_position[0], new_position[1], direction) in new_visits:
-                    loop_positions.append(visit)
-                    done = True
-                new_visits.append((new_position[0], new_position[1], direction))
-
-    print(len(set(loop_positions)))
+    # PART 2
+    # test:      6
+    # answer: 2143
+    print(count_loops(grid, start, guard_path))
