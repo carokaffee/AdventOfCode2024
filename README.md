56da6f503611703840c6d8a7bc6dc43e9ac2bb3a
carokaffee <carolin.kaffine@gmail.com>
2024-12-16 07:51:23 +0100

    day 15 both parts

--- README.md
+++ README.md
@@ -7,14 +7,14 @@ My solutions for AoC 2024 written in Python.
 | Day :christmas_tree: | Problem | Solution | | Day :christmas_tree: | Problem | Solution |
 | :---: | --- | --- | --- | :---: | --- | --- |
 | 01 | [Historian Hysteria](https://adventofcode.com/2024/day/1) | [day01.py](src/solutions/day01.py) || 14 | [Restroom Redoubt](https://adventofcode.com/2024/day/14) | [day14.py](src/solutions/day14.py) |
-| 02 | [Red-Nosed Reports](https://adventofcode.com/2024/day/2) | [day02.py](src/solutions/day02.py) || 15 | [](https://adventofcode.com/2024/day/15) | [](src/solutions/day15.py) |
-| 03 | [Mull It Over](https://adventofcode.com/2024/day/3) | [day03.py](src/solutions/day03.py) || 16 | [](https://adventofcode.com/2024/day/16) | [](src/solutions/day16.py) |
+| 02 | [Red-Nosed Reports](https://adventofcode.com/2024/day/2) | [day02.py](src/solutions/day02.py) || 15 | [Warehouse Woes](https://adventofcode.com/2024/day/15) | [day15.py](src/solutions/day15.py) |
+| 03 | [Mull It Over](https://adventofcode.com/2024/day/3) | [day03.py](src/solutions/day03.py) || 16 | [Reindeer Maze](https://adventofcode.com/2024/day/16) | [](src/solutions/day16.py) |
 | 04 | [Ceres Search](https://adventofcode.com/2024/day/4) | [day04.py](src/solutions/day04.py) || 17 | [](https://adventofcode.com/2024/day/17) | [](src/solutions/day17.py) |
 | 05 | [Print Queue](https://adventofcode.com/2024/day/5) | [day05.py](src/solutions/day05.py) || 18 | [](https://adventofcode.com/2024/day/18) | [](src/solutions/day18.py) |
 | 06 | [Guard Gallivant](https://adventofcode.com/2024/day/6) | [day06.py](src/solutions/day06.py) || 19 | [](https://adventofcode.com/2024/day/19) | [](src/solutions/day19.py) |
 | 07 | [Bridge Repair](https://adventofcode.com/2024/day/7) | [day07.py](src/solutions/day07.py) || 20 | [](https://adventofcode.com/2024/day/20) | [](src/solutions/day20.py) |
 | 08 | [Resonant Collinearity](https://adventofcode.com/2024/day/8) | [day08.py](src/solutions/day08.py) || 21 | [](https://adventofcode.com/2024/day/21) | [](src/solutions/day21.py) |
-| 09 | [Disk Fragmenter](https://adventofcode.com/2024/day/9) | [](src/solutions/day09.py) || 22 | [](https://adventofcode.com/2024/day/22) | [](src/solutions/day22.py) |
+| 09 | [Disk Fragmenter](https://adventofcode.com/2024/day/9) | [day09.py](src/solutions/day09.py) || 22 | [](https://adventofcode.com/2024/day/22) | [](src/solutions/day22.py) |
 | 10 | [Hoof It](https://adventofcode.com/2024/day/10) | [day10.py](src/solutions/day10.py) || 23 | [](https://adventofcode.com/2024/day/23) | [](src/solutions/day23.py) |
 | 11 | [Plutonian Pebbles](https://adventofcode.com/2024/day/11) | [day11.py](src/solutions/day11.py) || 24 | [](https://adventofcode.com/2024/day/24) | [](src/solutions/day24.py) |
 | 12 | [Garden Groups](https://adventofcode.com/2024/day/12) | [day12.py](src/solutions/day12.py) || 25 | [](https://adventofcode.com/2024/day/25) | [](src/solutions/day25.py) |
--- src/solutions/day15.py
+++ src/solutions/day15.py
@@ -1,7 +1,6 @@
 from src.tools.loader import load_data
 
 TESTING = False
-
 DIRECTIONS = {"^": (-1, 0), ">": (0, 1), "v": (1, 0), "<": (0, -1)}
 
 
@@ -9,13 +8,14 @@ def parse_input(data):
     grid = []
     moves = []
     for i, line in enumerate(data[0].split("\n")):
-        grid.append([])
+        grid_line = []
         for j, el in enumerate(line):
             if el == "@":
                 position = (i, j)
-                grid[-1].append(".")
+                grid_line.append(".")
             else:
-                grid[-1].append(el)
+                grid_line.append(el)
+        grid.append(grid_line)
 
     for line in data[1].split("\n"):
         for el in line:
@@ -24,122 +24,97 @@ def parse_input(data):
     return grid, moves, position
 
 
-def enlarge_grid(grid):
-    large_grid = []
-    for i, line in enumerate(grid):
-        large_grid.append([])
-        for j, el in enumerate(line):
-            if el == "#":
-                large_grid[-1].append("#")
-                large_grid[-1].append("#")
-            elif el == "O":
-                large_grid[-1].append("[")
-                large_grid[-1].append("]")
-            elif el == ".":
-                large_grid[-1].append(".")
-                large_grid[-1].append(".")
-            else:
-                large_grid[-1].append(".")
-                large_grid[-1].append(".")
-    return large_grid
-
-
 def print_grid(grid, position):
     for i, line in enumerate(grid):
         for j, el in enumerate(line):
-            if i == position[0] and j == position[1]:
+            if (i, j) == position:
                 print("@", end="")
             else:
                 print(el, end="")
         print()
 
 
-if __name__ == "__main__":
-    data = load_data(TESTING, "\n\n")
-    grid, moves, position = parse_input(data)
-
-    # print(position)
-    # print_grid(grid)
-    # print(moves)
-
+def solve_part_1(grid, moves, position):
     for move in moves:
         x, y = position
         dx, dy = move
-
-        # print(position)
-        # print_grid(grid)
-        # input()
-
         new_x, new_y = x + dx, y + dy
+
         if grid[new_x][new_y] == ".":
             position = (new_x, new_y)
-        elif grid[new_x][new_y] == "#":
-            pass
-        else:
+
+        if grid[new_x][new_y] == "O":
             next_elements = ["."]
             counter = 1
+
             while next_elements[-1] != "#":
                 next_elements.append(grid[x + counter * dx][y + counter * dy])
                 counter += 1
+
             box_counter = 1
             boxes_there = True
+
             while boxes_there:
                 if next_elements[box_counter] == "O":
                     box_counter += 1
                 else:
                     boxes_there = False
-            if next_elements[box_counter] == "#":
-                pass
-            elif next_elements[box_counter] == ".":
+
+            if next_elements[box_counter] == ".":
                 position = (new_x, new_y)
                 grid[new_x][new_y] = "."
                 grid[x + box_counter * dx][y + box_counter * dy] = "O"
+        else:
+            pass
+
+    return grid
+
+
+def enlarge_grid(grid):
+    large_grid = []
+    for line in grid:
+        grid_line = []
+        for el in line:
+            if el in "#.":
+                grid_line += [el] * 2
             else:
-                raise ValueError("something is wrong")
+                grid_line.append("[")
+                grid_line.append("]")
+        large_grid.append(grid_line)
+    return large_grid
 
-    score = 0
-    for i, line in enumerate(grid):
-        for j, el in enumerate(line):
-            if el == "O":
-                score += 100 * i + j
 
-    print("Score Part 1:", score)
-    # print()
-    # print_grid(grid)
-
-    grid, moves, position = parse_input(data)
+def solve_part_2(grid, moves, position):
     grid = enlarge_grid(grid)
     position = (position[0], position[1] * 2)
-
-    # print(position)
-    # print_grid(grid, position)
 
     for move in moves:
         x, y = position
         dx, dy = move
-
         new_x, new_y = x + dx, y + dy
+
         if grid[new_x][new_y] == ".":
             position = (new_x, new_y)
-        elif grid[new_x][new_y] == "#":
-            pass
-        else:
+
+        elif grid[new_x][new_y] in "[]":
             if dx == 0:
                 next_elements = ["."]
                 counter = 1
+
                 while next_elements[-1] != "#":
                     next_elements.append(grid[x + counter * dx][y + counter * dy])
                     counter += 1
+
                 box_counter = 1
                 boxes_there = True
+
                 while boxes_there:
                     if next_elements[box_counter] in ["[", "]"]:
                         box_counter += 1
                     else:
                         boxes_there = False
-                if next_elements[box_counter] == "#":
-                    pass
-                elif next_elements[box_counter] == ".":
+
+                if next_elements[box_counter] == ".":
                     position = (new_x, new_y)
                     grid[new_x][new_y] = "."
                     for i in range(2, box_counter + 1):
@@ -147,85 +122,81 @@ if __name__ == "__main__":
                             grid[x + i * dx][y + i * dy] = "[" if i % 2 == 0 else "]"
                         else:
                             grid[x + i * dx][y + i * dy] = "]" if i % 2 == 0 else "["
-                else:
-                    raise ValueError("something is wrong")
+
             else:
                 boxes_touched = []
                 queue = []
-                done = False
                 no_wall = True
+
                 if grid[new_x][new_y] == "[":
-                    if (((new_x, new_y), (new_x, new_y + 1))) not in boxes_touched:
-                        boxes_touched.append((((new_x, new_y), (new_x, new_y + 1))))
-                        queue.append((((new_x, new_y), (new_x, new_y + 1))))
+                    next_box = ((new_x, new_y), (new_x, new_y + 1))
+                    boxes_touched.append(next_box)
+                    queue.append(next_box)
                 elif grid[new_x][new_y] == "]":
-                    if (((new_x, new_y - 1), (new_x, new_y))) not in boxes_touched:
-                        boxes_touched.append((((new_x, new_y - 1), (new_x, new_y))))
-                        queue.append((((new_x, new_y - 1), (new_x, new_y))))
-                else:
-                    raise ValueError("not a correct box")
+                    next_box = ((new_x, new_y - 1), (new_x, new_y))
+                    boxes_touched.append(next_box)
+                    queue.append(next_box)
 
                 while queue:
-                    (bx, by), (cx, cy) = queue[0]
-                    if queue[0] == ((1, 13), (1, 14)):
-                        print("HERE WE ARE")
-                    if grid[bx + dx][by] == "#" or grid[cx + dx][cy] == "#":
+                    (x1, y1), (x2, y2) = queue[0]
+
+                    if grid[x1 + dx][y1] == "#" or grid[x2 + dx][y2] == "#":
                         queue = []
                         boxes_touched = []
                         no_wall = False
-                    else:
-                        if grid[bx + dx][by] == "[":
-                            if ((bx + dx, by), (bx + dx, by + 1)) not in boxes_touched:
-                                queue.append(((bx + dx, by), (bx + dx, by + 1)))
-                                boxes_touched.append(((bx + dx, by), (bx + dx, by + 1)))
-                            # queue = queue[1:]
-                        elif grid[bx + dx][by] == "]":
-                            if ((bx + dx, by - 1), (bx + dx, by)) not in boxes_touched:
-                                queue.append(((bx + dx, by - 1), (bx + dx, by)))
-                                boxes_touched.append(((bx + dx, by - 1), (bx + dx, by)))
-                            # queue = queue[1:]
-                        elif grid[bx + dx][by] == ".":
-                            pass
-                            # queue = queue[1:]
-                        else:
-                            raise ValueError("problem with left box")
 
-                        if grid[cx + dx][cy] == "[":
-                            if ((cx + dx, cy), (cx + dx, cy + 1)) not in boxes_touched:
-                                queue.append(((cx + dx, cy), (cx + dx, cy + 1)))
-                                boxes_touched.append(((cx + dx, cy), (cx + dx, cy + 1)))
-                            # queue = queue[1:]
-                        elif grid[cx + dx][cy] == "]":
-                            pass
-                        elif grid[cx + dx][cy] == ".":
-                            pass
-                            # queue = queue[1:]
-                        else:
-                            raise ValueError("problem with right box")
+                    else:
+                        if grid[x1 + dx][y1] == "[":
+                            next_box = ((x1 + dx, y1), (x1 + dx, y1 + 1))
+                            queue.append(next_box)
+                            boxes_touched.append(next_box)
+                        elif grid[x1 + dx][y1] == "]":
+                            next_box = ((x1 + dx, y1 - 1), (x1 + dx, y1))
+                            queue.append(next_box)
+                            boxes_touched.append(next_box)
+
+                        if grid[x2 + dx][y2] == "[":
+                            next_box = ((x2 + dx, y2), (x2 + dx, y2 + 1))
+                            queue.append(next_box)
+                            boxes_touched.append(next_box)
                         queue = queue[1:]
 
-                for (bx, by), (cx, cy) in reversed(boxes_touched):
-                    if grid[bx + dx][by] == "#" or grid[cx + dx][cy] == "#":
-                        # print(bx, dx, by)
-                        # print("queue", queue)
-                        # print("boxes touched", boxes_touched)
-                        # print_grid(grid, position)
-                        raise ValueError("what happens here?")
-                    grid[bx + dx][by] = "["
-                    grid[cx + dx][cy] = "]"
-                    grid[bx][by] = "."
-                    grid[cx][cy] = "."
+                for (x1, y1), (x2, y2) in reversed(boxes_touched):
+                    grid[x1 + dx][y1] = "["
+                    grid[x2 + dx][y2] = "]"
+                    grid[x1][y1] = "."
+                    grid[x2][y2] = "."
                 if no_wall:
                     position = (new_x, new_y)
 
-        # print(position)
-        # print_grid(grid, position)
-        # input()
+        else:
+            pass
 
+    return grid
+
+
+def calculate_score(grid):
     score = 0
     for i, line in enumerate(grid):
         for j, el in enumerate(line):
-            if el == "[":
+            if el in "O[":
                 score += 100 * i + j
+    return score
 
-    print("Score Part 2:", score)
+
+if __name__ == "__main__":
+    data = load_data(TESTING, "\n\n")
+
+    # PART 1
+    # test:     10092
+    # answer: 1442192
+    grid, moves, position = parse_input(data)
+    grid = solve_part_1(grid, moves, position)
+    print(calculate_score(grid))
+
+    # PART 2
+    # test:      9021
+    # answer: 1448458
+    grid, moves, position = parse_input(data)
+    grid = solve_part_2(grid, moves, position)
+    print(calculate_score(grid))
