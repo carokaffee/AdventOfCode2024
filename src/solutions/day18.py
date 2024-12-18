80f17edf03c724afed06fee4b50a9769e724412b
carokaffee <carolin.kaffine@gmail.com>
2024-12-18 07:00:00 +0100

    day 18

--- src/solutions/day18.py
+++ src/solutions/day18.py
@@ -1,7 +1,105 @@
 from src.tools.loader import load_data
+from collections import defaultdict
 
-TESTING = True
+TESTING = False
+
+MAX_X = 71 if not TESTING else 7
+MAX_Y = 71 if not TESTING else 7
+
+BYTE_LENGTH = 1024 if not TESTING else 12
+
+
+def parse_input(data):
+    bytes = []
+    for line in data:
+        bytes.append(tuple(map(int, line.split(","))))
+    return bytes
+
+
+def create_grid(bytes):
+    grid = []
+
+    for i in range(MAX_X):
+        line = ""
+        for j in range(MAX_Y):
+            if (i, j) in bytes:
+                line = line + "#"
+            else:
+                line = line + "."
+        grid.append(line)
+
+    return grid
+
+
+def get_neighbours(position, grid):
+    directions = ((0, 1), (1, 0), (-1, 0), (0, -1))
+    x, y = position
+    neighbours = []
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < MAX_X and 0 <= ny < MAX_Y and grid[nx][ny] != "#":
+            neighbours.append((nx, ny))
+
+    return neighbours
+
+
+def do_dijkstra(grid):
+    start = (0, 0)
+    end = (MAX_X - 1, MAX_Y - 1)
+    queue = [start]
+    distances = {(i, j): -1 for i in range(MAX_X) for j in range(MAX_Y)}
+    distances[start] = 0
+
+    while queue:
+        queue = sorted(queue, key=distances.__getitem__)
+        next = queue.pop(0)
+        neighbours = get_neighbours(next, grid)
+
+        for nx, ny in neighbours:
+            dist_neighbour = distances[(nx, ny)]
+            dist_next = distances[(next[0], next[1])]
+
+            if dist_next + 1 < dist_neighbour or dist_neighbour < 0:
+                distances[(nx, ny)] = dist_next + 1
+                queue.append((nx, ny))
+
+    return distances[end]
+
+
+def find_shortest_path(bytes):
+    grid = create_grid(bytes[:BYTE_LENGTH])
+    shortest_path = do_dijkstra(grid)
+    return shortest_path
+
+
+def find_blocking_byte(bytes):
+    done = False
+    counter = 1024 if not TESTING else 7
+
+    while not done:
+        counter += 1
+        # print("counter", counter)
+        new_bytes = tuple(bytes[:counter])
+
+        grid = create_grid(new_bytes)
+        result = do_dijkstra(grid)
+
+        if result == -1:
+            done = True
+
+    return bytes[counter - 1]
 
 
 if __name__ == "__main__":
     data = load_data(TESTING, "\n")
+    bytes = parse_input(data)
+
+    # PART 1
+    # test:    22
+    # answer: 312
+    print(find_shortest_path(bytes))
+
+    # PART 2
+    # test:     6,1
+    # answer: 28,26
+    print(find_blocking_byte(bytes))
