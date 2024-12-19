5d6925bcb68e625a1b2acae7729dab671a9a09d0
carokaffee <carolin.kaffine@gmail.com>
2024-12-19 09:01:54 +0100

    day 16 input works but test fails

--- src/solutions/day16.py
+++ src/solutions/day16.py
@@ -1,103 +1,97 @@
 from src.tools.loader import load_data
 
-TESTING = True
+TESTING = False
+DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+START_DIR = (0, 1)
+
+
+def find_start_and_end(maze):
+    start = [(i, j) for i in range(len(maze)) for j in range(len(maze[0])) if maze[i][j] == "S"][0]
+    end = [(i, j) for i in range(len(maze)) for j in range(len(maze[0])) if maze[i][j] == "E"][0]
+    return start, end
+
+
+def initialize(maze, start):
+    paths = dict()
+    previous = dict()
+
+    for i, row in enumerate(maze):
+        for j, el in enumerate(row):
+            if el == "#":
+                continue
+            for dir in DIRECTIONS:
+                paths[((i, j), dir)] = None
+                previous[((i, j), dir)] = []
+
+    paths[(start, START_DIR)] = 0
+
+    return paths, previous
 
 
 def get_neighbours(position, maze):
     x, y = position
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    neighbours = [(x + dx, y + dy) for dx, dy in directions if maze[x + dx][y + dy] != "#"]
+    neighbours = [(x + dx, y + dy) for dx, dy in DIRECTIONS if maze[x + dx][y + dy] != "#"]
     return neighbours
+
+
+def solve_maze(maze, start, end):
+    paths, previous = initialize(maze, start)
+    queue = [(start, START_DIR)]
+
+    while queue:
+        queue = sorted(queue, key=paths.__getitem__)
+        (x, y), current_dir = queue.pop(0)
+        neighbours = get_neighbours((x, y), maze)
+
+        for nx, ny in neighbours:
+            next_dir = (nx - x, ny - y)
+            rotation = next_dir != current_dir
+            step_cost = 1001 if rotation else 1
+            pos = ((x, y), current_dir)
+            next_pos = ((nx, ny), next_dir)
+            current_cost = paths[pos]
+
+            if paths[next_pos] is None:
+                paths[next_pos] = current_cost + step_cost
+                queue.append(next_pos)
+                previous[next_pos] = [pos]
+            else:
+                if current_cost + step_cost < paths[next_pos]:
+                    paths[next_pos] = current_cost + step_cost
+                    queue.append(next_pos)
+                    previous[next_pos] = [pos]
+                elif current_cost + step_cost == paths[next_pos]:
+                    previous[next_pos] += [pos]
+
+    shortest_path = min(paths[(end, dir)] for dir in DIRECTIONS if paths[(end, dir)] is not None)
+
+    return shortest_path, previous
+
+
+def find_best_path_points(previous, end):
+    best_points = set()
+    queue = [(end, START_DIR)]
+
+    while queue:
+        current = queue.pop(0)
+        best_points.add(current[0])
+        for prev in previous[current]:
+            queue.append(prev)
+
+    return len(best_points)
 
 
 if __name__ == "__main__":
     maze = load_data(TESTING, "\n")
-    print(maze)
+    start, end = find_start_and_end(maze)
+    shortest_path, previous = solve_maze(maze, start, end)
 
-    for i, row in enumerate(maze):
-        for j, el in enumerate(maze):
-            if maze[i][j] == "S":
-                start = (i, j)
-            elif maze[i][j] == "E":
-                end = (i, j)
+    # PART 1
+    # test:    11048
+    # answer: 102504
+    print(shortest_path)
 
-    print(start, end)
-    current_direction = (0, 1)
-
-    shortest_paths = dict()
-    previous = dict()
-    for i, row in enumerate(maze):
-        for j, el in enumerate(row):
-            if el != "#":
-                for dir in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-                    shortest_paths[((i, j), dir)] = None
-                    previous[((i, j), dir)] = []
-    shortest_paths[(start, current_direction)] = 0
-    # previous[(start, current_direction)] = 1
-
-    # print(shortest_paths)
-
-    queue = [(start, current_direction)]
-    found = False
-
-    while queue:
-        queue = sorted(queue, key=shortest_paths.__getitem__)
-        (x, y), current_direction = queue.pop(0)
-        dx, dy = current_direction
-        neighbours = get_neighbours((x, y), maze)
-        for nx, ny in neighbours:
-            next_direction = (nx - x, ny - y)
-            rotation = next_direction != current_direction
-
-            if shortest_paths[((nx, ny), next_direction)] is None:
-                if not rotation:
-                    shortest_paths[((nx, ny), next_direction)] = shortest_paths[((x, y), current_direction)] + 1
-                    queue.append(((nx, ny), next_direction))
-                else:
-                    shortest_paths[((nx, ny), next_direction)] = shortest_paths[((x, y), current_direction)] + 1001
-                    queue.append(((nx, ny), next_direction))
-                previous[((nx, ny), next_direction)] = [((x, y), current_direction)]
-            else:
-                if not rotation:
-                    if shortest_paths[((x, y), current_direction)] + 1 < shortest_paths[((nx, ny), current_direction)]:
-                        shortest_paths[((nx, ny), current_direction)] = shortest_paths[((x, y), current_direction)] + 1
-                        queue.append(((nx, ny), current_direction))
-                        previous[((nx, ny), current_direction)] = [((x, y), current_direction)]
-                    elif (
-                        shortest_paths[((x, y), current_direction)] + 1 == shortest_paths[((nx, ny), current_direction)]
-                    ):
-                        previous[((nx, ny), current_direction)] += [((x, y), current_direction)]
-                else:
-                    if shortest_paths[((x, y), current_direction)] + 1001 < shortest_paths[((nx, ny), next_direction)]:
-                        shortest_paths[((nx, ny), next_direction)] = shortest_paths[((x, y), current_direction)] + 1001
-                        queue.append(((nx, ny), (nx - x, ny - y)))
-                        previous[((nx, ny), next_direction)] = [((x, y), current_direction)]
-                    elif (
-                        shortest_paths[((x, y), current_direction)] + 1001 == shortest_paths[((nx, ny), next_direction)]
-                    ):
-                        previous[((nx, ny), next_direction)] += [((x, y), current_direction)]
-
-    shortest = 2**1000
-    for dir in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-        if (end, dir) in shortest_paths.keys():
-            print("end, dir", shortest_paths[(end, dir)])
-            if shortest_paths[(end, dir)] is not None:
-                if shortest_paths[(end, dir)] < shortest:
-                    shortest = shortest_paths[(end, dir)]
-                    shortest_coord = (end, dir)
-
-    print("Sol Part 1: ", shortest)
-
-    involved_points = set([end])
-    queue = [(end, dir)]
-
-    while queue:
-        current = queue.pop(0)
-        (x, y), dir = current
-        involved_points.add((x, y))
-        for prev in previous[current]:
-            queue.append(prev)
-
-    print("Sol Part 2: ", len(involved_points))
-
-    # 535
+    # PART 1
+    # test:    64
+    # answer: 535
+    print(find_best_path_points(previous, end))
