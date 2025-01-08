47e874322a5089a680b3ffba9b04479fc038b31a
carokaffee <carolin.kaffine@gmail.com>
2025-01-08 17:45:44 +0100

    day 22

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ My solutions for AoC 2024 written in Python.
 | 06 | [Guard Gallivant](https://adventofcode.com/2024/day/6) | [day06.py](src/solutions/day06.py) || 19 | [Linen Layout](https://adventofcode.com/2024/day/19) | [day19.py](src/solutions/day19.py) |
 | 07 | [Bridge Repair](https://adventofcode.com/2024/day/7) | [day07.py](src/solutions/day07.py) || 20 | [Race Condition](https://adventofcode.com/2024/day/20) | [day20.py](src/solutions/day20.py) |
 | 08 | [Resonant Collinearity](https://adventofcode.com/2024/day/8) | [day08.py](src/solutions/day08.py) || 21 | [Keypad Conundrum](https://adventofcode.com/2024/day/21) | [](src/solutions/day21.py) |
-| 09 | [Disk Fragmenter](https://adventofcode.com/2024/day/9) | [day09.py](src/solutions/day09.py) || 22 | [Monkey Market](https://adventofcode.com/2024/day/22) | [](src/solutions/day22.py) |
+| 09 | [Disk Fragmenter](https://adventofcode.com/2024/day/9) | [day09.py](src/solutions/day09.py) || 22 | [Monkey Market](https://adventofcode.com/2024/day/22) | [day22.py](src/solutions/day22.py) |
 | 10 | [Hoof It](https://adventofcode.com/2024/day/10) | [day10.py](src/solutions/day10.py) || 23 | [LAN Party](https://adventofcode.com/2024/day/23) | [day23.py](src/solutions/day23.py) |
 | 11 | [Plutonian Pebbles](https://adventofcode.com/2024/day/11) | [day11.py](src/solutions/day11.py) || 24 | [Crossed Wires](https://adventofcode.com/2024/day/24) | [day24.py](src/solutions/day24.py) |
 | 12 | [Garden Groups](https://adventofcode.com/2024/day/12) | [day12.py](src/solutions/day12.py) || 25 | [Code Chronicle](https://adventofcode.com/2024/day/25) | [day25.py](src/solutions/day25.py) |
--- src/solutions/day22.py
+++ src/solutions/day22.py
@@ -1,7 +1,86 @@
 from src.tools.loader import load_data
 
-TESTING = True
+TESTING = False
+PRUNE = 16777216
+
+
+def get_next_secret_number(a):
+    start = a
+    a *= 64
+    a ^= start
+    a %= PRUNE
+    start = a
+    a //= 32
+    a ^= start
+    a %= PRUNE
+    start = a
+    a *= 2048
+    a ^= start
+    a %= PRUNE
+    return a
+
+
+def solve_part_1(initial_secret_numbers):
+    score = 0
+
+    for num in initial_secret_numbers:
+        current = num
+        for _ in range(2000):
+            current = get_next_secret_number(current)
+        score += current
+
+    return score
+
+
+def solve_part_2(initial_secret_numbers):
+    best_score = 0
+    evolving_total = []
+    diffs_total = []
+    dicts = []
+
+    for num in initial_secret_numbers:
+        evolving_secret_numbers = [num % 10]
+        current = num
+        for _ in range(2000):
+            current = get_next_secret_number(current)
+            evolving_secret_numbers.append(current % 10)
+        diffs_total.append([evolving_secret_numbers[i + 1] - evolving_secret_numbers[i] for i in range(2000)])
+        seq = ()
+        seq_to_val = {}
+
+        for diff, val in zip(diffs_total[-1], evolving_secret_numbers[1:]):
+            seq = seq + (diff,)
+            if len(seq) > 4:
+                seq = seq[-4:]
+            if seq in seq_to_val:
+                continue
+            seq_to_val[seq] = val
+        evolving_total.append(evolving_secret_numbers)
+        dicts.append(seq_to_val)
+
+    possible_four_diffs = {tuple(diffs_total[i][j : j + 4]) for i in range(len(diffs_total)) for j in range(2000 - 4)}
+
+    for four_price_changes in possible_four_diffs:
+        score = 0
+        for seq_to_val in dicts:
+            score += seq_to_val.get(four_price_changes, 0)
+
+        if best_score < score:
+            best_score = score
+
+    return best_score
 
 
 if __name__ == "__main__":
     data = load_data(TESTING, "\n")
+    initial_secret_numbers = list(map(int, data))
+
+    # PART 1
+    # test:      37327623 # different test input
+    # answer: 18694566361
+    print(solve_part_1(initial_secret_numbers))
+
+    # PART 2
+    # test:     23
+    # answer: 2100
+    print(solve_part_2(initial_secret_numbers))
